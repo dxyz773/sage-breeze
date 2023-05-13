1443bd60df8643d54695b0b46c5da7f3d2637df5
demitryedwards <dxyz773@gmail.com>
2023-05-12 22:39:17 -0400

    table in readme test

--- script.js
+++ script.js
@@ -2,29 +2,30 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("form");
+
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const input = e.target.task_created.value;
-    createTask(input);
+    buildLi(input);
     form.reset();
   });
 });
 
-function createTask(input) {
-  const task = document.createElement("li");
-  task.textContent = `${input} `;
+function buildLi(input) {
+  const li = document.createElement("li");
+  li.textContent = `${input} `;
   const btn = document.createElement("button");
   btn.textContent = "x";
-  task.appendChild(btn);
+  li.appendChild(btn);
+  document.querySelector(".tasks").appendChild(li);
+  li.addEventListener("dblclick", turnPink);
   btn.addEventListener("click", deleteBtn);
-  task.addEventListener("dblclick", lettersPink);
-  document.querySelector(".tasks").appendChild(task);
 }
 
 function deleteBtn(btn) {
   btn.target.parentNode.remove();
 }
 
-function lettersPink(task) {
-  task.target.classList.add("pink_letters");
+function turnPink(letters) {
+  letters.target.classList.toggle("pink_letters");
 }
